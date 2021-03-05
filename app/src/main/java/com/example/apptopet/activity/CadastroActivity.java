9b7a8715d2f9134da0fc13483d36cc940f5edda0
marianatassan <marianatassan19@gmail.com>
2021-03-05 19:06:54 -0300

    Falta funcionalidade de postar.

--- app/src/main/java/com/example/apptopet/activity/CadastroActivity.java
+++ app/src/main/java/com/example/apptopet/activity/CadastroActivity.java
@@ -6,9 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.apptopet.MainActivity;
+import com.example.apptopet.LoginActivity;
 import com.example.apptopet.R;
+import com.example.apptopet.util.HttpRequest;
+import com.example.apptopet.util.Utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -17,12 +29,62 @@ public class CadastroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        Button btnCadastrar = findViewById(R.id.btnCadastrar);
+        Button btnCadastrar = findViewById(R.id.btnRegistrar);
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(CadastroActivity.this, MainActivity.class);
-                startActivity(i);
+                EditText etNovoLogin =  findViewById(R.id.etNovoLogin);
+                final String novoLogin = etNovoLogin.getText().toString();
+                if(novoLogin.isEmpty()) {
+                    Toast.makeText(CadastroActivity.this, "Campo de login não preenchido", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etNovaSenha =  findViewById(R.id.etNovaSenha);
+                final String novaSenha = etNovaSenha.getText().toString();
+                if(novaSenha.isEmpty()) {
+                    Toast.makeText(CadastroActivity.this, "Campo de senha não preenchido", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        HttpRequest httpRequest = new HttpRequest("https://servidor-topet-app.herokuapp.com/cadastro.php", "POST", "UTF-8");
+                        httpRequest.addParam("novoLogin", novoLogin);
+                        httpRequest.addParam("novaSenha", novaSenha);
+
+                        try {
+                            InputStream is = httpRequest.execute();
+                            String result = Utils.inputStream2String(is, "UTF-8");
+                            httpRequest.finish();
+
+                            JSONObject jsonObject = new JSONObject(result);
+                            final int success = jsonObject.getInt("success");
+                            if(success == 1) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(CadastroActivity.this, "Novo usuario registrado com sucesso", Toast.LENGTH_LONG).show();
+                                        finish();
+                                    }
+                                });
+                            }
+                            else {
+                                final String error = jsonObject.getString("message");
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(CadastroActivity.this, error, Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        } catch (IOException | JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/apptopet/util/Config.java
+++ app/src/main/java/com/example/apptopet/util/Config.java
@@ -1,4 +1,39 @@
 package com.example.apptopet.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class Config {
+    public static void setIdUsuario(Context context, Integer id) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.putInt("id", id).commit();
+    }
+
+    public static int getIdUsario(Context context) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        return mPrefs.getInt("id", 0);
+    }
+
+    public static void setLogin(Context context, String login) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.putString("login", login).commit();
+    }
+
+    public static String getLogin(Context context) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        return mPrefs.getString("login", "");
+    }
+
+    public static void setSenha(Context context, String senha) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.putString("senha", senha).commit();
+    }
+
+    public static String getSenha(Context context) {
+        SharedPreferences mPrefs = context.getSharedPreferences("configs", 0);
+        return mPrefs.getString("senha", "");
+    }
 }
